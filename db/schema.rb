038f17c5ce16be430e5f0f388dfb289b63004985
Min <madoka82@gmail.com>
2016-02-24 20:36:56 +0800

    Increase vote function

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160221121401) do
+ActiveRecord::Schema.define(version: 20160224120259) do
 
   create_table "topics", force: :cascade do |t|
     t.string   "title"
     t.text     "decription"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "topic_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
